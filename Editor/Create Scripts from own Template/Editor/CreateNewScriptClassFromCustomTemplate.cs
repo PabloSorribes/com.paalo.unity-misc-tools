e35dd781b6486bbe355ceb2307db874eb68266a5
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2020-02-15 01:27:35 +0100

    added misc tools from Outhere.core (such as create script from own template)

---
+++ Editor/Create Scripts from own Template/Editor/CreateNewScriptClassFromCustomTemplate.cs
@@ -0,0 +1,72 @@
+﻿//Source 1: https://forum.unity.com/threads/how-to-create-your-own-c-script-template.459977/#post-3533591 //Original code
+//Source 2: https://forum.unity.com/threads/how-to-create-your-own-c-script-template.459977/#post-3539041 //Micro Optimization
+//Source 3: https://forum.unity.com/threads/how-to-create-your-own-c-script-template.459977/#post-3569909 //What path to send in when the script is part of a Package
+
+using UnityEditor;
+using UnityEngine;
+
+public class CreateNewScriptClassFromCustomTemplate
+{
+	private static string pathToYourScriptTemplate = "com.paalo.unity-misc-tools/Editor/Create Scripts from own Template/ScriptTemplates/OutHereScriptTemplate.cs.txt";
+
+	[MenuItem(itemName: "Assets/Create/Create New Script from Custom Template", isValidateFunction: false, priority: 51)]
+	public static void CreateScriptFromTemplate()
+	{
+		int dialogResult = EditorUtility.DisplayDialogComplex(
+			"Create Script from Template",
+			"Choose the default template or specify another template?",
+			"Use Default",
+			"Cancel",
+			"Choose another file as template");
+
+		//Check if we're in the Package Development-project or in a project that is using this script as a Package.
+		Object scriptTemplateAsset = AssetDatabase.LoadAssetAtPath($"Packages/{pathToYourScriptTemplate}", typeof(Object));
+		if (scriptTemplateAsset == null)
+		{
+			scriptTemplateAsset = AssetDatabase.LoadAssetAtPath($"Assets/{pathToYourScriptTemplate}", typeof(Object));
+		}
+		pathToYourScriptTemplate = AssetDatabase.GetAssetPath(scriptTemplateAsset);
+
+
+		switch (dialogResult)
+		{
+			// Default template (ok)
+			case 0:
+				ProjectWindowUtil.CreateScriptAssetFromTemplateFile(pathToYourScriptTemplate, "OutHereBehaviour.cs");
+				break;
+
+			// Cancel.
+			case 1:
+				break;
+
+			// Choose another template (alternative)
+			case 2:
+				string templateAlternative = SelectScriptTemplateAsset();
+
+				if (string.IsNullOrEmpty(templateAlternative))
+				{
+					break;
+				}
+				ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templateAlternative, "OutHereBehaviour.cs");
+				break;
+
+			default:
+				Debug.LogError("Unrecognized option.");
+				break;
+		}
+	}
+
+	public static string SelectScriptTemplateAsset()
+	{
+		string path = EditorUtility.OpenFilePanel("SelectScriptTemplateAsset", "Assets", "");
+
+		if (path.Contains(Application.dataPath))
+		{
+			path = path.Replace(Application.dataPath, string.Empty).Trim();
+			path = path.TrimStart('/', '\\');
+			path = $"Assets/{path}";
+		}
+
+		return path;
+	}
+}
---
+++ Runtime/SineWaveMovement/SineWaveMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace OutHere
+{
+	public class SineWaveMovement : MonoBehaviour
+	{
+		Vector3 startPos;
+		public float amplitude = 5f;
+		public float period = 0.5f;
+
+		public enum SineModes
+		{
+			Normal,
+			BounceUp,
+			BounceUpWithPause,
+			UIAlertThrob
+		}
+		public SineModes sineModes = SineModes.Normal;
+
+		protected void Start()
+		{
+			startPos = transform.position;
+		}
+
+		protected void Update()
+		{
+			transform.position = GetSineWaveMovement(period, amplitude, startPos, Vector3.up);
+		}
+
+		private Vector3 GetSineWaveMovement(float period, float amplitude, Vector3 startPos, Vector3 direction)
+		{
+			float theta = Time.timeSinceLevelLoad / period;
+
+			float distance = 0f;
+
+			switch (sineModes)
+			{
+				case SineModes.Normal:
+					distance = amplitude * Mathf.Sin(theta);    //Up-down (normal sine wave)	
+					break;
+				case SineModes.BounceUp:
+					distance = amplitude * Mathf.Abs(Mathf.Sin(theta));     //Bounce Up (directly)
+					break;
+				case SineModes.BounceUpWithPause:
+					distance = amplitude * Mathf.Clamp01(Mathf.Sin(theta));     //Bounce Up, but pause for 2 periods between the bounces
+					break;
+				case SineModes.UIAlertThrob:
+					distance = amplitude * Mathf.Clamp(Mathf.Sin(theta), 0.6f, 1f);     //UI alert throb
+					break;
+				default:
+					break;
+			}
+
+			return startPos + direction * distance;
+		}
+	}
+}
