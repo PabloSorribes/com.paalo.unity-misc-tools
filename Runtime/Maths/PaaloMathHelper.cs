cdd9727d04cb956ac891a119e77a155de5857c0d
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2020-05-23 22:43:20 +0200

    Started adding a small maths library

---
+++ Runtime/Maths/PaaloMathHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Paalo.UnityMiscTools
+{
+	public static class PaaloMathHelper
+	{
+		/// <summary>
+		/// Remaps a value (x) in interval [A,B], to the proportional value in interval [C,D]
+		/// </summary>
+		/// <param name="x">The value to remap.</param>
+		/// <param name="A">the minimum bound of interval [A,B] that contains the x value</param>
+		/// <param name="B">the maximum bound of interval [A,B] that contains the x value</param>
+		/// <param name="C">the minimum bound of target interval [C,D]</param>
+		/// <param name="D">the maximum bound of target interval [C,D]</param>
+		public static float Remap(float x, float A, float B, float C, float D)
+		{
+			float remappedValue = C + (x-A)/(B-A) * (D - C);
+			return remappedValue;
+		}
+	}
+}
